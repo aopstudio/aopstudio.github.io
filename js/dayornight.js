f39d34bf381f36632f98a95445fe6b995dd59d1b
Cheng Yi <cenzhedong2@126.com>
2019-02-23 22:40:33 +0800

    实现根据当前时间自动切换日间模式和夜间模式，换了一种js事件绑定方式

--- js/dayornight.js
+++ js/dayornight.js
@@ -1,10 +1,29 @@
 
-document.getElementById("day").onclick=function(){
+function setDay(){
     document.getElementById("navbar").setAttribute("class","navbar navbar-default navbar-fixed-top");
     document.getElementById("dayornight").setAttribute("href","css/day.css");
-}
-document.getElementById("night").onclick=function(){
+};
+
+function setNight(){
     document.getElementById("navbar").setAttribute("class","navbar navbar-inverse navbar-fixed-top");
     document.getElementById("dayornight").setAttribute("href","css/night.css");
+};
+
+
+
+
+window.onload=function(){
+    var myDate=new Date();
+    var hour=myDate.getHours();
+    if(hour>21 || hours<7)
+    {
+        setNight();
+    }
+    else
+    {
+        setDay();
+    }
 }
+
+
 
