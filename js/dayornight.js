93f4948941e1afc729aa8fd281a7d5d041994183
Cheng Yi <cenzhedong2@126.com>
2019-02-23 22:14:12 +0800

    为日间和夜间模式专门写了css文件，使用js直接切换css文件，更好地支持夜间模式；支持背景粒子特效和鼠标的互动

--- js/dayornight.js
+++ js/dayornight.js
@@ -1,10 +1,10 @@
 
 document.getElementById("day").onclick=function(){
-    document.getElementById("particles-js").style.backgroundColor = 'rgb(88, 228, 221)';
     document.getElementById("navbar").setAttribute("class","navbar navbar-default navbar-fixed-top");
+    document.getElementById("dayornight").setAttribute("href","css/day.css");
 }
 document.getElementById("night").onclick=function(){
-    document.getElementById("particles-js").style.backgroundColor = 'rgb(0, 0, 0)';
     document.getElementById("navbar").setAttribute("class","navbar navbar-inverse navbar-fixed-top");
+    document.getElementById("dayornight").setAttribute("href","css/night.css");
 }
 
