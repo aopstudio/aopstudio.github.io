3737ab5e979bec7bfc12dd42b43bf12105f8aed6
Cheng Yi <cenzhedong2@126.com>
2019-04-03 08:36:25 +0800

    添加新的看板娘动作元素

--- js/waifu-tips.js
+++ js/waifu-tips.js
@@ -56,7 +56,7 @@ $('.waifu-tool .fui-user').click(function (){
 });
 
 $('.waifu-tool .fui-info-circle').click(function (){
-    window.open('https://www.cnblogs.com/yjlblog/p/8724881.html');
+    window.open('https://neusoftware.top/solo');
 });
 
 $('.waifu-tool .fui-cross').click(function (){
@@ -71,9 +71,10 @@ $('.waifu-tool .fui-photo').click(function (){
     window.Live2D.captureFrame = true;
 });
 
+
 function waifuWelcome(){
     var text;
-    var SiteIndexUrl = 'https://www.fghrsh.net/';  // 手动指定主页
+    var SiteIndexUrl = 'https://aopstudio.github.io/';  // 手动指定主页
     //var SiteIndexUrl = window.location.protocol+'//'+window.location.hostname+'/';  // 自动获取主页
 
     if (window.location.href == SiteIndexUrl) {      // 如果是主页
