3ae03337bb3172c3d11a9e8b7b8233edc735c0cb
Cheng Yi <cenzhedong2@126.com>
2019-04-09 19:18:00 +0800

    更新树与二叉树

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 60,
+        "value": 100,
         "density": {
           "enable": true,
           "value_area": 800
@@ -86,7 +86,7 @@ particlesJS('particles-js',
       "events": {
         "onhover": {
           "enable": true,
-          "mode": "grab"
+          "mode": "repulse"
         },
         "onclick": {
           "enable": true,
